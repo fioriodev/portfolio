4ddd1d08e4edea24b65de328f5337eb97c13e5af
fioriodev <guilherme.fiorio.dev@outlook.com>
2026-08-04 11:11:13 -0300

    feat: atualiza monorepo com novas melhorias

--- apps/fjusoccer/src/pages/home/index.tsx
+++ apps/fjusoccer/src/pages/home/index.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
+
 export function Home() {
     return (
         <>
             <h1>Página Home</h1>
+            <Link to="/login" className="text-white">
+                Ir para a página de Login
+            </Link>
         </>
     )
 }
